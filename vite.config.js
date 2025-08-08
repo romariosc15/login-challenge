9205f57d1d1a5f9b368ea93a2e9bb9db0001ca98
romariosc15 <romariosc15@outlook.com>
2025-08-08 13:30:02 -0500

    feat: add github pages deploy workflow yml

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-  //base: '/login-challenge/',
+  base: '/login-challenge/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
